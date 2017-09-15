7ec1c0bed8e6a3e05227e8b6bd21caf9a63461a6
Philipp Jahn <philipp.jahn@rollindonkey.com>
2017-09-15 11:47:16 +0200

    Project is now finished.

---
+++ Assets/Scripts/Tower/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum proType {
+	rock, arrow, fireball
+};
+
+public class Projectile : MonoBehaviour {
+
+	[SerializeField]
+	private int attackStrenght;
+
+	[SerializeField]
+	private proType projectileType;
+
+	public int AttackStrength {
+		get {
+			return attackStrenght;	
+		}
+	}
+
+	public proType ProjectileType {
+		get {
+			return projectileType;
+		}
+	}
+}
